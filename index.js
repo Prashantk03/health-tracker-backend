3b66ed14dac87784ee780a70d8f9370299f10b1b
Prashant Kumar <kumarprashant71037@gmail.com>
2025-06-11 14:25:43 +0530

    update

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
